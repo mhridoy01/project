f18aec1e1a1786307f7a37d8a86b56f829b37e01
Mehedi Ridoy <53517557+mhridoy01@users.noreply.github.com>
2022-10-24 23:06:20 +0600

    Add files via upload

---
+++ JS/button.js
@@ -0,0 +1,17 @@
+//console.log('button in js file');
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //console.log('submit button clicked');
+    window.location.href = 'reservation.html';
+})
+
+//console.log('button in js file');
+document.getElementById('btn-submit2').addEventListener('click', function(){
+    //console.log('submit button clicked');
+    window.location.href = 'reservation.html';
+})
+
+//console.log('button in js file');
+document.getElementById('btn-submit3').addEventListener('click', function(){
+    //console.log('submit button clicked');
+    window.location.href = 'reservation.html';
+})
